7cb5b85f43e59553bc098333717fd228db06bd17
AL-Khawaja <muhammadkhawaja@hotmail.com>
2023-04-07 11:24:45 +0300

    Validate

--- script.js
+++ script.js
@@ -11,3 +11,47 @@ function calculateGPA() {
   else alert("You should get ("+x.toFixed(2) +") in every course to let your GPA = "+expectedGPA);
 
 }
+
+function myFunction1() {
+  const input = document.getElementById("current-gpa");
+  input.value = input.value.replace(/[^0-9.]/g, "");
+
+  if(input.value >100 || input.value <60)
+  showAlert("Wrong GPA!");
+
+}
+function myFunction2() {
+  const input = document.getElementById("passed-credits");
+  input.value = input.value.replace(/[^0-9.]/g, "");
+
+  if(input.value <0 )
+  showAlert("Wrong passed credit hours");
+}
+
+function myFunction3() {
+
+  const input = document.getElementById("all-credits");
+  input.value = input.value.replace(/[^0-9.]/g, "");
+  if(input.value <0 )
+  showAlert("Wrong credit hours");
+
+}
+
+function myFunction4() {
+  const input = document.getElementById("expected-gpa");
+  input.value = input.value.replace(/[^0-9.]/g, "");
+  if(input.value >100 || input.value <60)
+  showAlert("Wrong Expected GPA!");
+}
+
+
+function showAlert(message) {
+  const alertBox = document.getElementById("myAlert");
+  alertBox.querySelector(".alert-message").textContent = message;
+  alertBox.style.display = "block";
+}
+
+function closeAlert() {
+  const alertBox = document.getElementById("myAlert");
+  alertBox.style.display = "none";
+}
